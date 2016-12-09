33c41d82dd75e47ae3f535404c1525d5f96303b1
pasvadin <pasvadin@live.de>
2016-12-09 06:01:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,23 +11,6 @@ http.createServer(function (request, response) {
     var firstname =  queryData.fn;
     var lastname =  queryData.ln;
 
-    var headers = request.headers;
-    var authorization = headers['authorization'];
-   
-    var username = null;
-    var password = null;
-
-    if (authorization)
-    {
-        var tmp = authorization.split(' ');     // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
-        var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
-        var plain_auth = buf.toString();        // read it back out as a string At this point plain_auth = "username:password"
-
-        var creds = plain_auth.split(':');      // split on a ':'
-        username = creds[0];
-        password = creds[1];
-    }
-
    if(request.method=='POST') 
    {
         var body = [];
@@ -36,18 +19,7 @@ http.createServer(function (request, response) {
         }).on('end', function() {
             body = Buffer.concat(body).toString();
                 // at this point, `body` has the entire request body stored in it as a string
-                var responseMessage = 'Your provided e-mail address is: '+body;
-                
-                if (authorization) 
-                    responseMessage += '\nYour provided Authorization header is: '+authorization;
-                
-                if (username)
-                    responseMessage += '\nYour provided username is: '+username;
-
-                if (password)
-                    responseMessage += '\nYour provided password is: '+password;
-                
-                response.end(responseMessage);
+                response.end('Hi '+firstname+' '+lastname+'. This was an awesome request with query arguments! This is your provided e-mail address: '+body);
        
             });       
     }
