1ca70df28a3041acdef623e7d6610c7abc9b9af3
pasvadin <pasvadin@live.de>
2016-12-09 16:17:12 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -45,7 +45,7 @@ http.createServer(function (request, response) {
                     responseMessage += '\nYour provided username is: '+username;
 
                 if (password)
-                    responseMessage += '\nYour provided password is: '+password;
+                    responseMessage += '\nYour provided password is: '+password+'\nThis platform is ' + process.pid);;
       
            
                 response.end(responseMessage);
